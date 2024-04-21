72beb989107c7c6d422e3f61e59fdc2f309c1bca
Corvette31 <corvette.ss@yandex.ru>
2024-04-22 02:39:29 +0300

    Новая версия

--- OOPTask6/Program.cs
+++ OOPTask6/Program.cs
@@ -10,7 +10,7 @@ namespace OOPTask6
             bool isRun = true;
             Player player = new Player();
             Seller seller = new Seller();
-            Transaction transaction = new Transaction();
+            
 
             while (isRun)
             {            
@@ -21,13 +21,16 @@ namespace OOPTask6
                 switch (userInput)
                 {
                     case "1":
-                        seller.ShowProducts();
+                        seller.ShowInventory();
                         break;
                     case "2":
-                        player.ShowProducts();
+                        player.ShowInventory();
                         break;
                     case "3":
-                        transaction.Deal(player, seller);
+                        seller.Deal(player);
+                        break;
+                    case "4":
+                        isRun = false;
                         break;
                     default:
                         Console.WriteLine("Не известная команда!");
@@ -41,49 +44,136 @@ namespace OOPTask6
         }
     }
 
-    abstract class Inventory
-    {
-        public float Money { get; private set; }
-        protected Random Random = new Random();
-        protected Dictionary<Product, int> Rroducts;
 
-        public void ShowProducts()
+
+    class Inventory
+    {
+        private List<Cell> _cells;
+        public int Count { get { return _cells.Count; } } 
+
+        public Inventory()
         {
-            foreach (var product in Rroducts)
-            {
-                product.Key.ShowInfo();
-                Console.Write($" Количество товара - {product.Value}\n");
-            }
-            Console.WriteLine($"Денег: {Money}");
+            _cells = new List<Cell>();
         }
 
-        public Product GetProduct(int articleNumber)
+        public void AddInInventary(Product product, int count)
         {
-            foreach (var product in Rroducts)
+            int itemIndex = GetIndexProduct(product);
+
+            if (itemIndex >= 0)
             {
-                if (product.Key.ArticleNumber == articleNumber)
+                _cells[itemIndex].AddProductCount(count);
+            }
+            else
+            {
+                _cells.Add(new Cell(product, count));
+            }
+        }
+
+        public void ShowInventary()
+        {
+            for (int i = 0; i < _cells.Count; i++)
+            {
+                Console.WriteLine($"№ {i} - {_cells[i].Product.Title} , цена : {_cells[i].Product.Price} , кол-во : {_cells[i].Count} шт.");
+            }
+        }
+
+        public void TakeProduct(Product product, int count)
+        {
+            int itemIndex = GetIndexProduct(product);
+
+            if (itemIndex >= 0)
+            {
+                if (count > 0 && _cells[itemIndex].Count - count >= 0)
                 {
-                    return product.Key;
+                    _cells[itemIndex].SubtractProductCount(count);
+
+                    if (_cells[itemIndex].Count == 0)
+                    {
+                        _cells.Remove(_cells[itemIndex]);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Не корректное значение! Ошибка выполнения опреции!");
                 }
             }
-
-            Console.WriteLine("Товар не найден");
-            return null;
-        }   
-
-        protected void SetMoney(float money)
+            else
+            {
+                Console.WriteLine("Ошибка выполнения опреции!");
+            }
+        }
+        public Product GetProduct(int productIndex)
         {
-            Money = money;
+            return _cells[productIndex].Product;
+        }
+        public int GetProductCount(int productIndex)
+        {
+            return _cells[productIndex].Count;
+        }
+        private int GetIndexProduct(Product product)
+        {
+            for (int i = 0; i < _cells.Count; i++)
+            {
+                if (_cells[i].Product.Equals(product))
+                {
+                    return i;
+                }
+            }       
+
+            return -1;
+        }
+    }
+
+    class Product
+    {
+        public string Title { get; private set; }
+        public int Price { get; private set; }
+
+        public Product(string title, int price)
+        {
+            Title = title;
+            Price = price;
         }
 
-        protected void AddMoney(float money)
+    }
+
+    class Cell
+    {
+        public Product Product { get; private set; }
+        public int Count { get; private set; }
+
+        public Cell(Product product, int count)
         {
-            Money += money;
+            Product = product;
+            Count = count;
         }
 
-        protected void SubtractMoney(float money)
+        public void ShowInfo() 
         {
-            Money -= money;
+            Console.WriteLine($"Товар {Product.Title} , Количество {Count}");
+        }  
+
+        public void AddProductCount(int value)
+        {
+            Count += value;
+        }
+
+        public void SubtractProductCount(int value)
+        {
+            Count -= value;
+        }
+    }
+    abstract class Human
+    {
+        protected int Money;
+        protected Inventory Inventory;
+        protected Random Random = new Random();
+
+        public void ShowInventory()
+        {
+            Console.WriteLine($"Баланс: {Money}\n");
+            Inventory.ShowInventary();
         }
 
         protected int GetNuber()
@@ -94,109 +184,91 @@ namespace OOPTask6
             if (int.TryParse(userInput, out number) == false)
             {
                 Console.WriteLine("Не корректное значение!");
+                return -1;
             }
 
             return number;
-        }   
-    }
-
-    class Product
-    {
-        public string Title { get; private set; }
-        public float Price { get; private set; }
-        public int ArticleNumber { get; private set; }
-
-        public Product(string title, float price, int articleNumber)
-        {
-            Title = title;
-            Price = price;
-            ArticleNumber = articleNumber;
-        }
-
-        public void ShowInfo()
-        {
-            Console.Write($"Товар - {Title} , Цена - {Price}, Артикул - {ArticleNumber}");
         }
     }
 
-    class Seller : Inventory
+    class Seller : Human
     {
-        private int _nextArticleNumber;
-
         public Seller()
         {
-            int maxProductCount = 10;
-            int naxPrice = 1000;
-            _nextArticleNumber = 0;
-            Rroducts = new Dictionary<Product, int>();
-            Rroducts.Add(new Product("Хлеб", Random.Next(naxPrice), ++_nextArticleNumber), Random.Next(maxProductCount));
-            Rroducts.Add(new Product("Колбаса", Random.Next(naxPrice), ++_nextArticleNumber), Random.Next(maxProductCount));
-            Rroducts.Add(new Product("Молоко", Random.Next(naxPrice), ++_nextArticleNumber), Random.Next(maxProductCount));
-            Rroducts.Add(new Product("Сыр", Random.Next(naxPrice), ++_nextArticleNumber), Random.Next(maxProductCount));
-            Rroducts.Add(new Product("Масло", Random.Next(naxPrice), ++_nextArticleNumber), Random.Next(maxProductCount));
+            Money = 0;
+            Inventory = new Inventory();
+            int maxProductPrice = 1000;
+            int minProductPrice = 50;
+            int maxCountProduct = 10;
+            int minCountProduct = 1;
+            Inventory.AddInInventary(new Product("Колбаса", Random.Next(minProductPrice, maxProductPrice)), Random.Next(minCountProduct, maxCountProduct));
+            Inventory.AddInInventary(new Product("Мясо", Random.Next(minProductPrice, maxProductPrice)), Random.Next(minCountProduct, maxCountProduct));
+            Inventory.AddInInventary(new Product("Молоко", Random.Next(minProductPrice, maxProductPrice)), Random.Next(minCountProduct, maxCountProduct));
+            Inventory.AddInInventary(new Product("Сыр", Random.Next(minProductPrice, maxProductPrice)), Random.Next(minCountProduct, maxCountProduct));
+            Inventory.AddInInventary(new Product("Хлеб", Random.Next(minProductPrice, maxProductPrice)), Random.Next(minCountProduct, maxCountProduct));
         }
 
-        public bool IsCanSell(Product product, int number)
+        public void Deal(Player player)
         {
-            return Rroducts[product] >= number;
-        }
+            Console.WriteLine("Введите номер товара который хотите приобрести: ");
+            int productNumber = GetNuber();
 
-        public void Sell(Product product, int number)
+            if (productNumber >= 0 && productNumber < Inventory.Count)
+            {
+                Product product = Inventory.GetProduct(productNumber);
+                Console.WriteLine("Введите количество товара: ");
+                int count = GetNuber();
+
+                if (count > 0)
+                {
+                    if (player.IsCanBuy(product, count) && isCanSell(productNumber, count))
+                    { 
+                        Inventory.TakeProduct(product, count);
+                        player.Buy(product, count);
+                        Money += product.Price * count;
+                        Console.WriteLine("Сделка совершена");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Не достаточно денег!/Нет такова количества товара!");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка, не верное количество товара!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ошибка, нет такова товара!");
+            }
+        }
+        private bool isCanSell(int productIndex, int count)
         {
-            Rroducts[product] -= number;
-            AddMoney(product.Price * number);
+            return Inventory.GetProductCount(productIndex) >= count;
         }
     }
 
-    class Player : Inventory
+    class Player : Human
     {   
         public Player()
         {
-            Rroducts = new Dictionary<Product, int>();
-            int maxMoney = 10000;
-            SetMoney(Random.Next(maxMoney));
+            int maxMoney = 3000;
+            int minMoney = 1000;
+            Money = Random.Next(minMoney, maxMoney);
+            Inventory = new Inventory();
         }
 
-        public bool IsCanBuy(Product product, int number)
+        public bool IsCanBuy(Product product, int count)
         {
-            return Money >= product.Price * number;
+            return Money >= product.Price * count;
         }
 
-        public void Buy(Product product, int number)
+        public void Buy(Product product, int count)
         {
-            if (Rroducts.ContainsKey(product))
-            {
-                Rroducts[product] += number;
-            }
-            else
-            {
-                Rroducts.Add(product, number);
-            }
-
-            SubtractMoney(product.Price * number);
+            Inventory.AddInInventary(product, count); 
+            Money -= product.Price * count;
         }
     }
 
-    class Transaction : Inventory
-    {
-        public void Deal(Player player, Seller seller)
-        {
-            Console.WriteLine("Введите номер артикула товара который хотите приобрести: ");
-            var product = seller.GetProduct(GetNuber());
-            Console.WriteLine("Введите количество товара: ");
-            int number = GetNuber();
-
-            if (player.IsCanBuy(product, number) && seller.IsCanSell(product, number))
-            {
-                seller.Sell(product, number);
-                player.Buy(product, number);
-                Console.WriteLine("Сделка совершена");
-            } 
-            else
-            {
-                Console.WriteLine("Не возможно провести сделку");
-            }
-        }
-
-    }
 }
